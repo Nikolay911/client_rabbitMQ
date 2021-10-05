05aae3f587b2b751288c5dea31b66af375f98ea2
Nikolay911 <81089615+Nikolay911@users.noreply.github.com>
2021-10-05 13:59:47 +0300

    Delete java/com/example/auction1_client_with_rabbitMQ directory

--- java/com/example/auction1_client_with_rabbitMQ/client_services/RestTemplateService.java
+++
@@ -1,9 +0,0 @@
-package com.example.auction1_client_with_rabbitMQ.client_services;
-
-import org.springframework.web.client.RestTemplate;
-
-public class RestTemplateService {
-
-    protected RestTemplate restTemplate = new RestTemplate();
-
-}
